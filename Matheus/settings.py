883146f2aadd53e447bcffc6332bc90124ee0474
eloas77 <matheus.dimon@gmail.com>
2023-10-25 15:08:12 -0300

    correção de erro sintático e configuração de urls

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'me,'
+    'me',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
