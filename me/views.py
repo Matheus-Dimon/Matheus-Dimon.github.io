b6fcd1281538a41e749c36c5b9c4dd9b5465ac5f
eloas77 <matheus.dimon@gmail.com>
2023-10-25 15:52:54 -0300

    correções em erros de sintaxe do html

--- me/views.py
+++ me/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'templates/index.html')
+    return render(request, 'index.html')
 
 def welcome(request):
-    return render(request, 'templates/welcome.html')
+    return render(request, 'welcome.html')
