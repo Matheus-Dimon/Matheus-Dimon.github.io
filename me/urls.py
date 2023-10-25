a2f5be696237cbad1426be5da40bab5136843692
eloas77 <matheus.dimon@gmail.com>
2023-10-25 15:24:50 -0300

    configuração de rotas,migrations,collectatic

--- me/urls.py
+++ me/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from me import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.welcome, name='welcome'),
+     path('me', views.index, name='index'),
     ]                                                    
--- me/views.py
+++ me/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'templates/index.html')
+
+def welcome(request):
+    return render(request, 'templates/welcome.html')
