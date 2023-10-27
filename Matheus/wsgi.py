ff2016c587182000e77d51c3388f5bf635d37033
eloas77 <matheus.dimon@gmail.com>
2023-10-27 19:17:43 -0300

    wsgi changes

--- Matheus/wsgi.py
+++ Matheus/wsgi.py
@@ -2,13 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-from Matheus import MyWSGIApp
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Matheus.settings')
 application = get_wsgi_application()
-app = application
-
-
+application = WhiteNoise(application, root="/caminho/para/arquivos/estaticos")
+application.add_files("/caminho/para/mais/arquivos/estaticos", prefix="mais-arquivos/")
