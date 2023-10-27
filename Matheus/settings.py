7f10dc9e44f15b1c530284c28f8e699cc68d53f0
eloas77 <matheus.dimon@gmail.com>
2023-10-27 19:36:01 -0300

    teststes de configuração

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -103,8 +103,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 MEDIA_URL = '/img/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
