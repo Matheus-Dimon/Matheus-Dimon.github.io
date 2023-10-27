675b19e72af66de6bd5060b79decdf3f22f173f7
eloas77 <matheus.dimon@gmail.com>
2023-10-27 16:46:27 -0300

    ajustes nas configurações

--- me/urls.py
+++ me/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import URLPattern, path
 from me import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+if settings.DEBUG:
+    URLPattern += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
