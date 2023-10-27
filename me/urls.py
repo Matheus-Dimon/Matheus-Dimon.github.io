148eae49bf5638e0070c256a4154e0723d660cbf
eloas77 <matheus.dimon@gmail.com>
2023-10-27 16:54:57 -0300

    undo

--- me/urls.py
+++ me/urls.py
@@ -3,9 +3,6 @@ from me import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    URLPattern += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
