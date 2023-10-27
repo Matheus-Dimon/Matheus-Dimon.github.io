1008e47bdb0de81fd88e0e17aa148bff23fa2e46
eloas77 <matheus.dimon@gmail.com>
2023-10-27 19:52:09 -0300

    teste de configuração static host

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -102,9 +102,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = '/static/'
 MEDIA_URL = '/img/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = STATIC_HOST + "/static/"
