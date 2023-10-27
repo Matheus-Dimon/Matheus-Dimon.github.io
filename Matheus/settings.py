92ece8e33769f98f0cf22a95446c7c4b5f9eba17
eloas77 <matheus.dimon@gmail.com>
2023-10-27 18:30:09 -0300

    setting changes

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -114,13 +114,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("image/png", ".png", True)
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STORAGES = {
-    
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- Matheus/wsgi.py
+++ Matheus/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
-from Matheus import MyWSGIApp
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Matheus.settings')
 
 application = get_wsgi_application()
 app = application
 
-application = MyWSGIApp()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
