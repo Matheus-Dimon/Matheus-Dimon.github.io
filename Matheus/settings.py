9fdb575c7e6387ff51958193238fa6947683e436
eloas77 <matheus.dimon@gmail.com>
2023-10-28 02:40:31 -0300

    config

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -108,7 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS [
-     (os.path.join(BASE_DIR, 'static'),)
-     ]
+STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
+     
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
--- Matheus/wsgi.py
+++ Matheus/wsgi.py
@@ -1,13 +1,12 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from Matheus import MyWSGIApp
 
-application = MyWSGIApp()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Matheus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quiz.settings')
+
 application = get_wsgi_application()
-app = application
 
+# vercel config
+
+app = application # add this line.
 
