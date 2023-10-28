2c6bc1f13839bb110d74037f77b46a3966bc540b
eloas77 <matheus.dimon@gmail.com>
2023-10-28 02:24:00 -0300

    wsgi correção

--- Matheus/wsgi.py
+++ Matheus/wsgi.py
@@ -1,6 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from Matheus import MyWSGIApp
 
+application = MyWSGIApp()
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Matheus.settings')
 application = get_wsgi_application()
 app = application
