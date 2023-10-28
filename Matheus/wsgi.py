648f3f881d1bbd761c9dce6fbfa4db9ecea2a40f
eloas77 <matheus.dimon@gmail.com>
2023-10-28 02:41:41 -0300

    correção

--- Matheus/wsgi.py
+++ Matheus/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quiz.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Matheus.settings')
 
 application = get_wsgi_application()
 
