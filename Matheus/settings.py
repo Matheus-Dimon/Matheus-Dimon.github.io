d0b0dfb0580e0eba217815ca7fefe3910d40ff5c
eloas77 <matheus.dimon@gmail.com>
2023-10-27 23:00:32 -0300

    teste 2

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -106,9 +106,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 MEDIA_URL = '/img/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 if DEBUG:
   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
