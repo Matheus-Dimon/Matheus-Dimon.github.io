6e2c0981403ccc9b7bfb4cb3f9ae771597c4d3d8
eloas77 <matheus.dimon@gmail.com>
2023-10-27 21:40:13 -0300

    tentando outra configuração

--- Matheus/settings.py
+++ Matheus/settings.py
@@ -110,3 +110,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
+'DIRS': [os.path.join(BASE_DIR,'templates')]
