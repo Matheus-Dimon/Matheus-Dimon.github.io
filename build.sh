d97bac71c0e1ee941f3d3571e089e975b336e6c2
eloas77 <matheus.dimon@gmail.com>
2023-11-03 20:08:38 -0300

    sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-python3.9 pip install -U pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.12 pip install -U pip
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
