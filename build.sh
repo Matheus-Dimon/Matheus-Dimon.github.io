c8fff5f5645e66cd6c5baa02574a909b919e9940
eloas77 <matheus.dimon@gmail.com>
2023-11-03 20:10:04 -0300

    sh2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-python3.12 pip install -U pip
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3 pip install -U pip
+python3 -m pip install -r requirements.txt
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
