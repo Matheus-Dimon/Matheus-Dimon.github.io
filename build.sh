b9dddeeb881cc4d0a7c2dcfa2de86e83599f5d24
eloas77 <matheus.dimon@gmail.com>
2023-11-03 19:45:54 -0300

    remove no input from build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ python3.12 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate 
 #Install whitenoise
 python3.12 manage.py whitenoise
 # Collect staticfiles
