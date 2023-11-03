0326e3d7f1219049c72e1b26cbcb6b604ee51472
eloas77 <matheus.dimon@gmail.com>
2023-11-03 20:07:22 -0300

    tentativa 1

--- Matheus/urls.py
+++ Matheus/urls.py
@@ -6,7 +6,4 @@ import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('me.urls')),  
-
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-
-# Update pip
-echo "Updating pip..."
-python3.12 pip install -U pip
-
-# Install dependencies
-echo "Installing project dependencies..."
-python3.12 -m pip install -r requirements.txt
-
-# Make migrations
-echo "Making migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3.9 pip install -U pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
