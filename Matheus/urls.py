faa32160f250aff1cfed47d2e405dbc26f1c5ef0
eloas77 <matheus.dimon@gmail.com>
2023-11-03 20:04:03 -0300

    build sh

--- Matheus/urls.py
+++ Matheus/urls.py
@@ -5,4 +5,8 @@ import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('me.urls')),  ]
+    path('', include('me.urls')),  
+
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
