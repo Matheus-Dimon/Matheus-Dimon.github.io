1f7eec29aa5ac40586b12a43ecd819397728fbef
eloas77 <matheus.dimon@gmail.com>
2023-11-03 19:57:08 -0300

    urls changes

--- Matheus/urls.py
+++ Matheus/urls.py
@@ -5,7 +5,4 @@ import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('me.urls')),  
-]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('', include('me.urls')),  ]
