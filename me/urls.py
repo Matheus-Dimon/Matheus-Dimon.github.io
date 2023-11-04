acf3bb3fe66cd16a6904b202eb0c7c06f4a3ecb2
eloas77 <matheus.dimon@gmail.com>
2023-11-03 21:15:36 -0300

    git hub example

--- me/urls.py
+++ me/urls.py
@@ -8,3 +8,6 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
      path('me', views.index, name='index'),
     ]                                                    
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
