d95803a4d52bc181f3a6858c128dbfd90709c389
eloas77 <matheus.dimon@gmail.com>
2023-11-03 21:36:42 -0300

    build sh

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Update pip
+echo "Updating pip..."
+python3.12 pip install -U pip
+
+# Install dependencies
+echo "Installing project dependencies..."
+python3.12 -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+
+#Install whitenoise
+echo "Installing white noise..."
+python3.12 manage.py whitenoise
+
+#Collect static
+echo "Collect static..."
+python3.12 manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"
